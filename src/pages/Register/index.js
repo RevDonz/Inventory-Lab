8587d4f7b3fd8af72da8ab4ac99dadcb868e3b7b
Alif Babrizq <alifbabrizq@.com>
2021-07-24 09:30:11 +0700

    tampilan-register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -71,36 +71,36 @@ const Register = () => {
   }
   
   return (
-    <div className="bg-blue-100">
+    <div className="bg-indigo-100">
       <svg width="378" height="561" viewBox="0 0 378 561" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M-85.3893 348.371C-83.0144 357.843 -79.8072 367.337 -75.7339 376.725C-43.8456 450.22 29.0497 489.388 87.0823 464.208C135.883 443.035 158.633 383.101 145.675 320.865C145.8 297.105 154.773 245.801 189.67 230.66L203.312 224.74C230.748 212.836 249.95 188.682 259.202 158.754C269.52 125.379 267.463 84.8237 250.651 46.0746C218.762 -27.4205 145.867 -66.5881 87.8344 -41.4087L-215.426 90.1711C-273.459 115.351 -294.653 195.342 -262.765 268.837C-230.876 342.332 -157.981 381.5 -99.9485 356.32C-94.8019 354.087 -89.9451 351.423 -85.3893 348.371Z" fill="#9EB4FA"/>
       </svg>
       <div className="-mt-96 font-poppins">
         <div className="items-center justify-center w-9/12 text-center m-auto">
-          <h1 className="text-5xl md:text-7xl pb-10 text-blue-500 font-normal">Registrasi</h1>
+          <h1 className="text-5xl md:text-7xl pb-10 text-indigo-900 font-semibold">Registrasi</h1>
         </div>
         <div className="w-full pt-11 text-lg">
           <form onSubmit={onSubmit}>
             <div className="mx-10 items-center justify-center text-center">
-              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg my-4" type="text" name="fullname" placeholder="Nama Lengkap" 
+              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg my-4 font-semibold" type="text" name="fullname" placeholder="Nama Lengkap" 
               value={fullname} onChange={(e) => setFullname(e.target.value)}/> <br/>
-              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg my-4" type="text" name="NIM" placeholder="Nomor Induk Mahasiswa / NIM" 
+              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg my-4 font-semibold" type="text" name="NIM" placeholder="Nomor Induk Mahasiswa / NIM" 
               value={NIM} onChange={(e) => setNIM(e.target.value)}/> <br/>
-              <input className="w-full py-3 md:py-3 md:w-1/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg md:rounded-r-none my-4" type="text" name="major" placeholder="Jurusan"
+              <input className="w-full py-3 md:py-3 md:w-1/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg md:rounded-r-none my-4 font-semibold" type="text" name="major" placeholder="Jurusan"
               value={major} onChange={(e) => setMajor(e.target.value)}/>
-              <input className="w-full py-3 md:py-3 md:w-1/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg md:rounded-none my-4" type="text" name="faculty" placeholder="Fakultas"
+              <input className="w-full py-3 md:py-3 md:w-1/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg md:rounded-none my-4 font-semibold" type="text" name="faculty" placeholder="Fakultas"
               value={faculty} onChange={(e) => setFaculty(e.target.value)}/>
-              <input className="w-full py-3 md:py-3 md:w-1/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg md:rounded-l-none my-4" type="text" name="year" placeholder="Angkatan"
+              <input className="w-full py-3 md:py-3 md:w-1/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg md:rounded-l-none my-4 font-semibold" type="text" name="year" placeholder="Angkatan"
               value={year} onChange={(e) => setYear(e.target.value)}/> <br/>
-              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg my-4" type="number" name="phoneNumber" placeholder="Nomor Handphone"
+              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg my-4 font-semibold" type="number" name="phoneNumber" placeholder="Nomor Handphone"
               value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/> <br/>
-              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg my-4" type="text" name="homeAddress" placeholder="Alamat"
+              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg my-4 font-semibold" type="text" name="homeAddress" placeholder="Alamat"
               value={homeAddress} onChange={(e) => setHomeAddress(e.target.value)}/> <br/>
-              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg my-4" type="email" name="email" placeholder="E-mail"
+              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg my-4 font-semibold" type="email" name="email" placeholder="E-mail"
               value={email} onChange={(e) => setEmail(e.target.value)}/> <br/>
-              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg my-4" type="password" name="password" placeholder="Kata Sandi"
+              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg my-4 font-semibold" type="password" name="password" placeholder="Kata Sandi"
               value={password} onChange={(e) => setPassword(e.target.value)}/> <br/>
-              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-blue-200 rounded-lg my-4" type="password" name="confirmPass" placeholder="Konfirmasi Kata Sandi"/> <br/>
+              <input className="w-full py-3 md:py-3 md:w-3/5 focus:ring-blue-300 focus:border-blue-300 border-1 border-gray-300 rounded-lg my-4 font-semibold" type="password" name="confirmPass" placeholder="Konfirmasi Kata Sandi"/> <br/>
             </div>
             <div className="px-10 md:w-3/5 m-auto pt-9">
               <p>Apakah Kamu Asisten Laboratorium? </p>
@@ -130,11 +130,11 @@ const Register = () => {
               </div>
               <label className="inline-flex items-center mt-4" >
                 <input className="focus:ring-blue-300 h-5 w-5 border-blue-300 rounded" type="checkbox"/>
-                <span className="ml-4 text-blue-400 text-base md:text-lg">Saya Telah Membaca dan Menyetujui Persyaratan Layanan</span> 
+                <span className="ml-4 text-blue-900 text-base md:text-lg font-semibold">Saya Telah Membaca dan Menyetujui Persyaratan Layanan</span> 
               </label>
             </div>
             <div className="items-center text-center mt-16 pb-20 md:pb-0">
-              <button className="bg-blue-300 text-white-100 rounded-lg px-20 py-4 text-2xl shadow-2xl">Daftar</button>
+              <button className="bg-blue-600 text-white rounded-lg px-20 py-4 text-2xl shadow-2xl">Daftar</button>
             </div>
           </form>
         </div>
