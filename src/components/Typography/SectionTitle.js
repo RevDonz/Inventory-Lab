9eafd3facda5e7b91c5fd51783f3c7c00893e9be
RevDonz <aprilio842@gmail.com>
2021-08-23 10:53:04 +0700

    Menambahkan typograhpy pagetitle

---
+++ src/components/Typography/SectionTitle.js
@@ -0,0 +1,5 @@
+const SectionTitle = ({ children }) => {
+    return <h2 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">{children}</h2>
+}
+
+export default PageTitle
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Alert from './Alert'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 import Content from './Content'
+import PageTitle from './Typography'
+import SectionTitle from './Typography'
 
-export { Alert, Navbar, Sidebar, Content }
+export { Alert, Navbar, Sidebar, Content, PageTitle, SectionTitle }
