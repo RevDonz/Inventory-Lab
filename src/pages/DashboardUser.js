02e0627317d914a28392b1213935f4c6970ccad6
RevDonz <aprilio842@gmail.com>
2021-09-04 14:41:14 +0700

    Ubah banner dashboard

--- src/pages/DashboardUser.js
+++ src/pages/DashboardUser.js
@@ -49,64 +49,13 @@ const DashboardUser = () => {
             <PageTitle>List Barang</PageTitle>
             <SectionTitle>Barang Yang Paling Di Cari</SectionTitle>
 
-            {/* <div className='grid grid-cols-3 gap-2 sm:gap-6'>
-                <div className='bg-white col-span-2 rounded-lg p-3 border shadow-md'>
-                    <div className='flex items-center justify-center'>
-                        <div className='w-1/2 space-y-2 md:space-y-3 px-1 md:px-3'>
-                            <h1 className='md:font-semibold md:text-xl'>
-                                {dataMax.itemName}
-                            </h1>
-                            <div className="flex flex-col md:flex-row md:space-x-2 font-normal text-gray-700 text-xs md:text-sm py-2">
-                                <p>
-                                    {dataMax.itemInBorrow} Dipinjam
-                                </p>
-                                <p>
-                                    {dataMax.itemAmount - dataMax.itemInBorrow} Tersisa
-                                </p>
-                            </div>
-                            <button
-                                type='submit'
-                                onClick={() =>
-                                    history.push(
-                                        `/app/user/pengajuan/${dataMax._id}`
-                                    )
-                                }
-                                className='hover:bg-indigo-800 bg-indigo-700 text-white text-sm px-2 py-1 md:px-3 md:py-2 rounded-md'
-                            >
-                                Pinjam
-                            </button>
-                        </div>
-                        <div className='w-1/2'>
-                            <div className=''>
-                                <img
-                                    className='rounded-md object-cover'
-                                    src={dataMax.itemPicture}
-                                    alt={dataMax.itemName}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='bg-gray-700 rounded-lg border shadow-md px-3 py-3 sm:px-7 sm:py-5 flex-wrap content-center grid'>
-                    <h1 className='text-white text-center text-xs md:text-xl md:font-semibold'>
-                        Minjem ga ribet, dengan Inventory Lab
-                    </h1>
-                    <img
-                        className='h-20 sm:w-auto sm:h-52 object-cover object-center justify-self-center'
-                        alt=''
-                        src={saly}
-                    ></img>
-                </div>
-            </div> */}
-
             <div className='flex justify-center space-x-3 items-stretch'>
                 <div className='border bg-white w-2/3 rounded-md flex items-center'>
-                    <div className='flex flex-col w-2/3 pl-5'>
-                        <h1 className='md:font-semibold md:text-xl'>
+                    <div className='flex flex-col w-1/2 md:w-2/3 pl-5'>
+                        <h1 className='md:font-semibold text-sm md:text-xl'>
                             {dataMax.itemName}
                         </h1>
                         <div className='flex flex-col mb-2 md:flex-row md:space-x-2 font-normal text-gray-700 text-xs md:text-sm py-2'>
-                            <p>{dataMax.itemInBorrow} Dipinjam</p>
                             <p>
                                 {dataMax.itemAmount - dataMax.itemInBorrow}{' '}
                                 Tersisa
@@ -124,23 +73,25 @@ const DashboardUser = () => {
                     <div className='w-1/3'>
                         <div className='items-center justify-center flex'>
                             <img
-                                className='rounded-md h-1/2 w-1/2'
+                                className='rounded-md w-full h-full'
                                 src={dataMax.itemPicture}
                                 alt={dataMax.itemName}
                             />
                         </div>
                     </div>
                 </div>
-                <div className='border bg-gray-700 w-1/3 rounded-md'>
-                    <h1 className='text-white text-center text-xs md:text-xl md:font-semibold'>
-                        Minjem ga ribet, dengan Inventory Lab
-                    </h1>
-                    <div className='items-center justify-center flex'>
-                        <img
-                            className='rounded-md h-1/2 w-1/2'
-                            src={saly}
-                            alt='banner'
-                        />
+                <div className='border bg-gray-700 w-1/2 md:w-1/3 rounded-md'>
+                    <div className="flex flex-col">
+                        <h1 className='text-white text-center text-xs md:text-lg pt-3'>
+                            Minjem ga ribet, dengan Inventory Lab
+                        </h1>
+                        <div className='items-center justify-center flex'>
+                            <img
+                                className='rounded-md max-w-sm pb-3 w-36 h-36'
+                                src={saly}
+                                alt='banner'
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -168,7 +119,6 @@ const DashboardUser = () => {
                                         {item.itemName}
                                     </h3>
                                     <div className='text-gray-500 flex flex-col text-xs mb-2'>
-                                        <p>{item.itemInBorrow} Dipinjam</p>
                                         <p>
                                             {item.itemAmount -
                                                 item.itemInBorrow}{' '}
@@ -183,7 +133,7 @@ const DashboardUser = () => {
                                                     `/app/user/pengajuan/${item._id}`
                                                 )
                                             }
-                                            className='bg-indigo-600 hover:bg-indigo-700 text-white text-sm rounded-md px-3 py-2'
+                                            className='bg-indigo-600 hover:bg-indigo-700 text-white text-sm rounded-md p-2'
                                         >
                                             Pinjam
                                         </button>
