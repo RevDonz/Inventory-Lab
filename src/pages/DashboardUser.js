248b8bcc909c5e46bdb79fce0f78e8e788bc1ea3
RevDonz <aprilio842@gmail.com>
2021-09-04 14:00:52 +0700

    Ubah banner dashboard

--- src/pages/DashboardUser.js
+++ src/pages/DashboardUser.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { PageTitle, SectionTitle } from '../components';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { saly } from '../assets';
 
 const DashboardUser = () => {
@@ -49,14 +49,14 @@ const DashboardUser = () => {
             <PageTitle>List Barang</PageTitle>
             <SectionTitle>Barang Yang Paling Di Cari</SectionTitle>
 
-            <div className='grid grid-cols-3 gap-2 sm:gap-6'>
-                <div className='bg-blue-600 col-span-2 rounded-lg p-3'>
+            {/* <div className='grid grid-cols-3 gap-2 sm:gap-6'>
+                <div className='bg-white col-span-2 rounded-lg p-3 border shadow-md'>
                     <div className='flex items-center justify-center'>
                         <div className='w-1/2 space-y-2 md:space-y-3 px-1 md:px-3'>
-                            <h1 className='text-white md:font-semibold md:text-xl'>
+                            <h1 className='md:font-semibold md:text-xl'>
                                 {dataMax.itemName}
                             </h1>
-                            <div className="flex flex-col md:flex-row md:space-x-2 font-normal text-gray-300 text-xs md:text-sm py-2">
+                            <div className="flex flex-col md:flex-row md:space-x-2 font-normal text-gray-700 text-xs md:text-sm py-2">
                                 <p>
                                     {dataMax.itemInBorrow} Dipinjam
                                 </p>
@@ -71,13 +71,13 @@ const DashboardUser = () => {
                                         `/app/user/pengajuan/${dataMax._id}`
                                     )
                                 }
-                                className='hover:bg-indigo-900 bg-indigo-700 text-white text-sm px-2 py-1 md:px-3 md:py-2 rounded-md'
+                                className='hover:bg-indigo-800 bg-indigo-700 text-white text-sm px-2 py-1 md:px-3 md:py-2 rounded-md'
                             >
                                 Pinjam
                             </button>
                         </div>
                         <div className='w-1/2'>
-                            <div className='aspect-w-8 aspect-h-8'>
+                            <div className=''>
                                 <img
                                     className='rounded-md object-cover'
                                     src={dataMax.itemPicture}
@@ -87,8 +87,8 @@ const DashboardUser = () => {
                         </div>
                     </div>
                 </div>
-                <div className='bg-gray-700 rounded-lg sm:rounded-2xl px-3 py-3 sm:px-7 sm:py-5 flex-wrap content-center grid'>
-                    <h1 className='text-white text-center text-xs sm:text-xl sm:font-semibold'>
+                <div className='bg-gray-700 rounded-lg border shadow-md px-3 py-3 sm:px-7 sm:py-5 flex-wrap content-center grid'>
+                    <h1 className='text-white text-center text-xs md:text-xl md:font-semibold'>
                         Minjem ga ribet, dengan Inventory Lab
                     </h1>
                     <img
@@ -96,6 +96,52 @@ const DashboardUser = () => {
                         alt=''
                         src={saly}
                     ></img>
+                </div>
+            </div> */}
+
+            <div className='flex justify-center space-x-3 items-stretch'>
+                <div className='border bg-white w-2/3 rounded-md flex items-center'>
+                    <div className='flex flex-col w-2/3 pl-5'>
+                        <h1 className='md:font-semibold md:text-xl'>
+                            {dataMax.itemName}
+                        </h1>
+                        <div className='flex flex-col mb-2 md:flex-row md:space-x-2 font-normal text-gray-700 text-xs md:text-sm py-2'>
+                            <p>{dataMax.itemInBorrow} Dipinjam</p>
+                            <p>
+                                {dataMax.itemAmount - dataMax.itemInBorrow}{' '}
+                                Tersisa
+                            </p>
+                        </div>
+                        <div className=''>
+                            <Link
+                                to={`/app/user/pengajuan/${dataMax._id}`}
+                                className='hover:bg-indigo-800 bg-indigo-700 text-white text-sm px-2 py-1 md:px-3 md:py-2 rounded-md'
+                            >
+                                Pinjam
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='w-1/3'>
+                        <div className='items-center justify-center flex'>
+                            <img
+                                className='rounded-md h-1/2 w-1/2'
+                                src={dataMax.itemPicture}
+                                alt={dataMax.itemName}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className='border bg-gray-700 w-1/3 rounded-md'>
+                    <h1 className='text-white text-center text-xs md:text-xl md:font-semibold'>
+                        Minjem ga ribet, dengan Inventory Lab
+                    </h1>
+                    <div className='items-center justify-center flex'>
+                        <img
+                            className='rounded-md h-1/2 w-1/2'
+                            src={saly}
+                            alt='banner'
+                        />
+                    </div>
                 </div>
             </div>
 
@@ -118,7 +164,9 @@ const DashboardUser = () => {
                                     />
                                 </div>
                                 <div className='group px-3 pt-2 flex flex-col'>
-                                    <h3 className='text-sm font-semibold md:text-lg'>{item.itemName}</h3>
+                                    <h3 className='text-sm font-semibold md:text-lg'>
+                                        {item.itemName}
+                                    </h3>
                                     <div className='text-gray-500 flex flex-col text-xs mb-2'>
                                         <p>{item.itemInBorrow} Dipinjam</p>
                                         <p>
